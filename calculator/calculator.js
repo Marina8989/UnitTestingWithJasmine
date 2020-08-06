74aceb6f02768f701a482c43bf95c25a04b19c40
Marina8989 <marina.mill209@gmail.com>
2020-08-06 16:54:19 -0700

    added js, now to testing

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -30,9 +30,9 @@ function setupIntialValues() {
   const rate = document.querySelector('#loan-rate');
 
   //to each variable assign a value from obj
-  amount = obj.amount;
-  years = obj.years;
-  rate = obj.rate;
+  amount.value = obj.amount;
+  years.value = obj.years;
+  rate.value = obj.rate;
 
   //call funtion update() here
   update();
@@ -54,10 +54,19 @@ function update() {
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-  
+  //create a variable and assign to it a object's rate divided by 100 and result devided by 12
+    const interestRate = values.rate / 100 / 12;
+  //create a variable and multiply the object's years by 12 and floor the result
+    const numberOfPayments = Math.floor(values.years * 12);
+  //return the result (check the formula provided for reference:   result = 𝑃×𝑖 / 1−(1+𝑖)−𝑛
+  return values.amount * interestRate / (1 - Math.pow(1 + interestRate), numberOfPayments).toFixed(2);         
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+ // create a variable and assign to it an html element with id monthly-payment 
+  const monthlyPayment = document.querySelector('#monthly-payment');
+//take that variable and get the text value to display : dollars sign concat with the function's parameter
+monthlyPayment.innerText = `$${monthly}`;  
 }
